581c2a083d05dab369a3f7c1dea6835585e38de9
Sam Lee <sflee36@yahoo.com.au>
2020-02-16 11:21:58 +0800

    2nd commit

--- learnphp.php
+++ learnphp.php
@@ -10,9 +10,13 @@
     date_default_timezone_set("UTC");
 
     echo date("h:i:s:u a, l F jS Y e");
-    echo "</p>";
+    
+    $usersName = $_POST['username'];
+    $streetsAddress = $_POST['streetaddress'];
+    $cityAddress = $_POST['cityaddress'];
+    echo "<p>$usersName </br> $streetsAddress </br> $cityAddress </br></p>";
 
-    ?>;
+    ?>
 
 </body>
 </html>
